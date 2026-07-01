c5c24c6449fb0aee6619402fed0c058cd9ccb7ec
wygnd <wygndo@gmail.com>
2026-07-01 07:21:03 +0300

    fix: FilesModule - add url to connection to file service by grpc

--- gateway/src/modules/files/module.ts
+++ gateway/src/modules/files/module.ts
@@ -6,16 +6,18 @@ import { join } from 'node:path';
 import { FileControllerV1 } from '@modules/files/controllers/controller';
 import { FileService } from '@modules/files/services/service';
 import { FileGrpcProvider } from '@modules/files/providers';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ClientsModule.registerAsync([
       {
         name: FILE_SERVICE,
-        useFactory: () => ({
+        useFactory: (config: ConfigService) => ({
           name: FILE_SERVICE,
           transport: Transport.GRPC,
           options: {
+            url: config.getOrThrow('API_GATEWAY_FILE_SERVICE_URL'),
             package: FILE_PACKAGE_NAME,
             protoPath: join(
               process.cwd(),
@@ -27,6 +29,7 @@ import { FileGrpcProvider } from '@modules/files/providers';
             ),
           },
         }),
+        inject: [ConfigService],
       },
     ]),
   ],
